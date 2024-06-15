cc7ad248949fcf2b3e50b0a772af089b63d9558b
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2024-06-15 22:55:03 +0700

    fix: navbar style

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
   return (
-    <h1 className="text-red-400">Home</h1>
+    <></>
   )
 }
--- src/components/front/announcement.tsx
+++ src/components/front/announcement.tsx
@@ -1,7 +1,7 @@
 export default function FrontAnnouncement() {
     return (
-        <section className="w-full pt-4 px-4">
-            <div className="container h-[56px] flex items-center justify-center bg-black rounded-[20px]">
+        <section className="w-full pt-4 pb-4 px-4">
+            <div className="container h-[56px] w-full flex items-center justify-center bg-black rounded-[20px]">
                 <h2 className="text-base text-white text-center">Use Code <strong className="font-extrabold italic">THESALE</strong> to Get 20% Discount</h2>
             </div>
         </section>
