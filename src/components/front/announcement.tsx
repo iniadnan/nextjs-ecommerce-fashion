af5f87477032341569b4a2b334384217c57240ff
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2024-06-17 07:53:39 +0700

    fix: announcement.tsx responsive

--- src/components/front/announcement.tsx
+++ src/components/front/announcement.tsx
@@ -1,8 +1,8 @@
 export default function FrontAnnouncement() {
     return (
-        <section className="w-full pt-4 pb-4 px-4">
-            <div className="container h-[56px] w-full flex items-center justify-center bg-[#111111] rounded-[20px]">
-                <h2 className="text-base text-white text-center">Use Code <strong className="font-extrabold italic">THESALE</strong> to Get 20% Discount</h2>
+        <section className="w-full pt-2.5 md:pt-4 pb-2.5 md:pb-4 px-2.5 md:px-4">
+            <div className="container h-[50px] sm:h-[56px] w-full flex items-center justify-center bg-[#111111] rounded-[16px] md:rounded-[20px]">
+                <h2 className="text-sm sm:text-base text-white text-center">Use Code <strong className="font-extrabold italic">THESALE</strong> to Get 20% Discount</h2>
             </div>
         </section>
     )
