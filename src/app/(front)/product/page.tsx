ab665cf8c93eefb121604826523c94251559c07f
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2024-06-23 23:29:04 +0700

    change: the meta title

--- src/app/(front)/product/page.tsx
+++ src/app/(front)/product/page.tsx
@@ -7,7 +7,7 @@ import type { Metadata } from "next";
 import "./index.css"
 
 export const metadata: Metadata = {
-    title: "Products"
+    title: "Products Fashion"
 };
 
 export default function Page() {
