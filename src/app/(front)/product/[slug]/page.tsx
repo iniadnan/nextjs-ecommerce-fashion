7647206e58b3245be8f38f28d9126ffdc7b476bb
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2024-06-24 17:25:34 +0700

    text: new commit to trigger workflows

--- src/app/(front)/product/[slug]/page.tsx
+++ src/app/(front)/product/[slug]/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Detail Product"
+    title: "Detail Product - git"
 };
 
 export default function Page() {
